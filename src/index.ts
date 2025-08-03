11e68b35395643516b33f6970fceede9d7249125
KatherineJoelle <53322457+KJwalker3D@users.noreply.github.com>
2025-08-03 19:34:50 +0200

    new approach, prev not working in prod

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,33 @@
 import { Color4, Vector3 } from '@dcl/sdk/math'
 import { engine, InputAction, Material, MeshCollider, MeshRenderer, pointerEventsSystem, Transform } from '@dcl/sdk/ecs'
 import { syncEntity, isStateSyncronized } from '@dcl/sdk/network'
-import { MessageBus } from '@dcl/sdk/message-bus'
 
 const colors = [Color4.Black(), Color4.White(), Color4.Red(), Color4.Green(), Color4.Blue(), Color4.Yellow(), Color4.Magenta(), Color4.fromHexString('#00FFFF')]
 let currentColorIndex = 0
 let cubeCreated = false
-const sceneMessageBus = new MessageBus()
-
-// Uses syncEntity for multiplayer sync, fallback to MessageBus for local testing
-// syncEntity is the DCL-compliant way to sync entities between clients
-// MessageBus is a fallback for local testing
 
 export function main() {
   engine.addSystem(() => {
-    if (!cubeCreated) {
-      // Try DCL-compliant sync first, fallback to MessageBus for local testing
-      if (isStateSyncronized()) {
-        createSyncedCube()
-      } else {
-        // Fallback: create cube with MessageBus for local testing
-        createMessageBusCube()
-      }
-    }
-  })
-}
-
-function createSyncedCube() {
-  const cube = engine.addEntity()
-  MeshRenderer.setBox(cube)
-  MeshCollider.setBox(cube)
-  Transform.create(cube, { position: Vector3.create(8, 1, 8) })
-  Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
-  
-  syncEntity(cube, [Material.componentId], 1)
-  
-  pointerEventsSystem.onPointerDown(
-    { entity: cube, opts: { button: InputAction.IA_PRIMARY, hoverText: 'Click to change color' } },
-    () => {
-      currentColorIndex = (currentColorIndex + 1) % colors.length
+    if (isStateSyncronized() && !cubeCreated) {
+      const cube = engine.addEntity()
+      MeshRenderer.setBox(cube)
+      MeshCollider.setBox(cube)
+      Transform.create(cube, { position: Vector3.create(8, 1, 8) })
       Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
+      
+      // Mark entity as synced - only sync the Material component since that's what changes
       syncEntity(cube, [Material.componentId], 1)
+      
+      pointerEventsSystem.onPointerDown(
+        { entity: cube, opts: { button: InputAction.IA_PRIMARY, hoverText: 'Click to change color' } },
+        () => {
+          currentColorIndex = (currentColorIndex + 1) % colors.length
+          Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
+          syncEntity(cube, [Material.componentId], 1)
+        }
+      )
+      
+      cubeCreated = true
     }
-  )
-  
-  cubeCreated = true
-}
-
-function createMessageBusCube() {
-  const cube = engine.addEntity()
-  MeshRenderer.setBox(cube)
-  MeshCollider.setBox(cube)
-  Transform.create(cube, { position: Vector3.create(8, 1, 8) })
-  Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
-  
-  pointerEventsSystem.onPointerDown(
-    { entity: cube, opts: { button: InputAction.IA_PRIMARY, hoverText: 'Click to change color' } },
-    () => {
-      currentColorIndex = (currentColorIndex + 1) % colors.length
-      Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
-      sceneMessageBus.emit('colorChange', { colorIndex: currentColorIndex })
-    }
-  )
-  
-  sceneMessageBus.on('colorChange', (data: { colorIndex: number }) => {
-    currentColorIndex = data.colorIndex
-    Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
   })
-  
-  cubeCreated = true
 }
