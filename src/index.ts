9b0437ff385d51f763e62bfd9418800c4b175e9e
KatherineJoelle <53322457+KJwalker3D@users.noreply.github.com>
2025-08-03 20:01:01 +0200

    prev test not working in prod. new test

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,27 @@ import { syncEntity, isStateSyncronized } from '@dcl/sdk/network'
 
 const colors = [Color4.Black(), Color4.White(), Color4.Red(), Color4.Green(), Color4.Blue(), Color4.Yellow(), Color4.Magenta(), Color4.fromHexString('#00FFFF')]
 let currentColorIndex = 0
-let cubeCreated = false
 
 export function main() {
-  engine.addSystem(() => {
-    if (isStateSyncronized() && !cubeCreated) {
-      const cube = engine.addEntity()
-      MeshRenderer.setBox(cube)
-      MeshCollider.setBox(cube)
-      Transform.create(cube, { position: Vector3.create(8, 1, 8) })
-      Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
-      
-      // Mark entity as synced - only sync the Material component since that's what changes
-      syncEntity(cube, [Material.componentId], 1)
-      
-      pointerEventsSystem.onPointerDown(
-        { entity: cube, opts: { button: InputAction.IA_PRIMARY, hoverText: 'Click to change color' } },
-        () => {
-          currentColorIndex = (currentColorIndex + 1) % colors.length
-          Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
-          syncEntity(cube, [Material.componentId], 1)
-        }
-      )
-      
-      cubeCreated = true
+  // Create cube immediately for all players
+  const cube = engine.addEntity()
+  MeshRenderer.setBox(cube)
+  MeshCollider.setBox(cube)
+  Transform.create(cube, { position: Vector3.create(8, 1, 8) })
+  Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
+  
+  // Mark entity as synced - only sync the Material component since that's what changes
+  syncEntity(cube, [Material.componentId], 1000)
+  
+  pointerEventsSystem.onPointerDown(
+    { entity: cube, opts: { button: InputAction.IA_PRIMARY, hoverText: 'Click to change color' } },
+    () => {
+      // Only sync color changes when state is synchronized
+      if (isStateSyncronized()) {
+        currentColorIndex = (currentColorIndex + 1) % colors.length
+        Material.setBasicMaterial(cube, { diffuseColor: colors[currentColorIndex] })
+        syncEntity(cube, [Material.componentId], 1000)
+      }
     }
-  })
+  )
 }
